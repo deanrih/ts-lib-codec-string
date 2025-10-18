3f007cee031017207eda448746f393a96969f53d
deanrih <deanrih@users.noreply.github.com>
2025-10-19 06:39:02 +0700

    fix: missing example
    Signed-off-by: deanrih <deanrih@users.noreply.github.com>

--- example/index.example.ts
+++ example/index.example.ts
@@ -1,1 +1,9 @@
-import { } from "../src";
+import { base32Decode, base32Encode } from "../src";
+
+const plainText = "Hello World!";
+const encoded = base32Encode(plainText);
+const decoded = base32Decode(encoded);
+
+console.log(encoded);
+console.log(decoded);
+console.log(decoded.toString("utf8"));
